7150d1e48adf5623010bd37f7ca7d62f06d30952
sheikh.kaiyum <sheikh.kaiyum@fintechhub.com.bd>
2025-05-04 22:56:50 +0600

    two_sum Done

--- Easy/Arrays/001_Two_Sum/Program.cs
+++ Easy/Arrays/001_Two_Sum/Program.cs
@@ -2,10 +2,57 @@
 {
     internal class Program
     {
+        //Complexity=O(n)^2
         private static int[] TwoSum(int[] nums, int target)
         {
+            for (int i = 0; i < nums.Length; i++) { 
+                for (int j = 1; j <= nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[]{i, j};
+                    }
 
-            return new int[nums.Length];
+                }
+            }
+
+            return new int[];
+        }
+
+        //Complexity=O(n)
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+
+                Dictionary<int, int> result = new Dictionary<int, int>();
+                int i = 0;
+                while (i < nums.Length)
+                {
+                    int dif = target - nums[i];
+                    if (result.ContainsKey(dif))
+                    {
+                        return new int[] { result[dif], i };
+                    }
+                    result.Add(nums[i], i);
+                    i++;
+                }
+
+                return new int[] { };
+            }
+
+            //Dictionary<int, int> map = new Dictionary<int, int>();
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    int t = target - nums[i];
+            //    if (map.ContainsKey(t))
+            //    {
+            //        return new int[] { map[t], i };
+            //    }
+            //    map[nums[i]] = i;
+            //}
+
+            //return new int[] { };
         }
         static void Main(string[] args)
         {
--- Easy/Arrays/001_Two_Sum/obj/Release/net8.0/001_Two_Sum.AssemblyInfo.cs
+++ Easy/Arrays/001_Two_Sum/obj/Release/net8.0/001_Two_Sum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001_Two_Sum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12cbd4de3efdf39cfddd4be55725a76bce052f20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c289bf4d0ed7a8fdc0539deefbfef27e61c63e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("001_Two_Sum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001_Two_Sum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
